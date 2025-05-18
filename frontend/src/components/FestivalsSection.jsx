0313f5d0c176167527d991d4470a011937fda6c5
Idris732 <111731282+Idris732@users.noreply.github.com>
2025-05-18 22:54:14 +0200

    Lite endringer for å gjør koden semantisk

--- frontend/src/components/FestivalsSection.jsx
+++ frontend/src/components/FestivalsSection.jsx
@@ -4,12 +4,12 @@ export default  function FestivalsSection({ event }) {
   const imageUrl = event.images?.[0]?.url;
 
   return (
-    <div className="event-card">
+    <article className="event-card">
       <img src={imageUrl} alt={event.name} />
       <h3>{event.name}</h3>
       <Link to={`/event/${event.id}`} className="btn">
         Les mer om {event.name}
       </Link>
-    </div>
+    </article>
   );
 }
