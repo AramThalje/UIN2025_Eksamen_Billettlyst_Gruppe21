5a634e590082b01cca039381c3d2b9a667749767
Kudu4Ever <111729786+Kudu4Ever@users.noreply.github.com>
2025-05-18 22:13:12 +0200

    Ticketmaster: gjær lite endringer på api "fetchFestivalsById"

--- frontend/src/api/ticketmaster.js
+++ frontend/src/api/ticketmaster.js
@@ -1,22 +1,22 @@
-// src/api/ticketmaster.js
 
-// Din Ticketmaster API-nøkkel
+
+// Din Ticketmaster API-nøkkel, Her brukte vi Aram sin apikey for å hente info fra ticketmaster api
 const API_KEY = 'OJWIEjGGbVgjMgWLS99KuhEdrPtRGiLh';
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2';
 
-// Standardfestivaler
+// Spesifikk festivaler som skal vises øvert på Home seksjon
 const FESTIVAL_IDS = [
-  'Z698xZb_Z16v7eGkFy', // Findings
-  'Z698xZb_Z17q339',   // Neon
-  'Z698xZb_Z17qfaA',   // Skeikampenfestivalen
-  'Z698xZb_Z17q3qg'    // Tons of Rock
+  'Z698xZb_Z16v7eGkFy', //  Id-en av festival: Findings
+  'Z698xZb_Z17q339',   // Id-en av festival: Neon
+  'Z698xZb_Z17qfaA',   // Id-en av festival: Skeikampenfestivalen
+  'Z698xZb_Z17q3qg'    // Id-en av festival: Tons of Rock
 ];
 
 /* Henter festivaler ved angitte Ticketmaster event IDs */
 export async function fetchFestivalsById(
-  ids = FESTIVAL_IDS
+  Fes_ids = FESTIVAL_IDS
 ) {
-  const urls = ids.map(
+  const urls = Fes_ids.map(
     (id) => `${BASE_URL}/events/${id}.json?apikey=${API_KEY}`
   );
 
@@ -33,42 +33,6 @@ export async function fetchFestivalsById(
   return events;
 }
 
-/* Henter spesifiserte festivaler etter navn */
-export async function fetchFestivalsByName(
-  names = FESTIVAL_NAMES
-) {
-  const queries = names.map((name) => {
-    const params = new URLSearchParams({
-      apikey: API_KEY,
-      keyword: name,
-      countryCode: 'NO',
-      size: '1',
-    });
-    return `${BASE_URL}/events.json?${params.toString()}`;
-  });
-
-  const responses = await Promise.all(
-    queries.map((url) => fetch(url))
-  );
-  const datas = await Promise.all(
-    responses.map((res) => {
-      return res.json();
-    })
-  );
-
-  const events = datas
-    .map((data, idx) => {
-      const targetName = names[idx].toLowerCase();
-      const matches = data._embedded?.events || [];
-      return (
-        matches.find((e) => e.name.toLowerCase() === targetName) ||
-        matches[0]
-      );
-    })
-    .filter(Boolean);
-
-  return events;
-}
 
 /* Hent alle arrangementer i en by */
 export async function fetchEventsByCity(city) {
